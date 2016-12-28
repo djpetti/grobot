17aba69098bcb3d808f450008143efe589380fd5
Daniel Petti <djpetti@gmail.com>
2016-12-28 18:27:24 -0500

    Add code and tests for reading messages from MCU.

--- python/serial_talker.py
+++ python/serial_talker.py
@@ -1,7 +1,9 @@
 """ Communicate with the MCUs connected via the serial interface. """
 
 
-import functools
+import collections
+import copy
+import enum
 import logging
 
 import serial
@@ -9,6 +11,114 @@ import serial
 
 logger = logging.getLogger(__name__)
 
+
+class _Parser:
+  """ Parses raw messages, and emits Message instances. """
+
+  class State(enum.Enum):
+    """ Keeps track of the parser state. """
+    READING_START = "READING_START"
+    READING_COMMAND = "READING_COMMAND"
+    READING_SOURCE = "READING_SOURCE"
+    READING_DEST = "READING_DEST"
+    READING_FIELDS = "READING_FIELDS"
+    DONE = "DONE"
+
+  def __init__(self):
+    # Any data that we've received, but haven't finished parsing yet.
+    self.__unused = ""
+    # The complete messages that were received.
+    self.__parsed = collections.deque()
+
+    # Keeps track of the state of the parser.
+    self.__state = self.State.READING_START
+    # The current message that we are working on.
+    self.__message = Message()
+
+  def __parse_char(self, char):
+    """ Parses a single character.
+    Args:
+      char: The character to parse. """
+    if (char == '<' and self.__state != self.State.READING_START):
+      # If we get an open-bracket, that means that, regardless of where we are,
+      # we've found the start of a message.
+      logger.warning("Dropping partial message.")
+      message = Message()
+
+    if self.__state == self.State.READING_START:
+      # Looking for the leading <.
+      if char == '<':
+        # We found the start.
+        self.__state = self.State.READING_COMMAND
+
+    elif self.__state == self.State.READING_COMMAND:
+      # All bytes read are now part of the command, until we hit a slash.
+      if char == '/':
+        # We're done reading the comand.
+        self.__state = self.State.READING_SOURCE
+      else:
+        self.__message.command += char
+
+    elif self.__state == self.State.READING_SOURCE:
+      # The source should be a single char.
+      self.__message.source = int(char)
+      self.__state = self.State.READING_DEST
+
+    elif self.__state == self.State.READING_DEST:
+      if char == '/':
+        # The source and dest are separated from the rest of the message with a
+        # slash.
+        self.__state = self.State.READING_FIELDS
+        # The parser needs an empty string to start with when parsing fields.
+        self.__message.fields.append("")
+      elif char == '>':
+        # Some valid messages have no fields.
+        self.__state = self.State.DONE
+      else:
+        # The dest should also be a single character.
+        self.__message.dest = int(char)
+
+    elif self.__state == self.State.READING_FIELDS:
+      # All bytes are part of this particular field, until we hit a control
+      # character.
+      if char == '/':
+        # We hit the end of this field.
+        self.__message.fields.append("")
+      elif char == '>':
+        # We hit the end of the message.
+        self.__state = self.State.DONE
+      else:
+        self.__message.fields[-1] += char
+
+    # This is technically not part of the above if-else block. We want to run it
+    # in EVERY case when we're in the done state.
+    if self.__state == self.State.DONE:
+      # We're done parsing the message. Save the complete message and reset.
+      logger.debug("Finished parsing message.")
+
+      self.__parsed.appendleft(copy.deepcopy(self.__message))
+      self.__message = Message()
+
+      self.__state = self.State.READING_START
+
+  def parse(self, data):
+    """ Parse some new data.
+    Args:
+      data: The raw data to parse. """
+    logger.debug("Parsing new data: %s" % (data))
+
+    for char in data:
+      self.__parse_char(char)
+
+  def get_message(self):
+    """
+    Returns:
+      The earliest message received, or None if there are no new messages. """
+    if not len(self.__parsed):
+      # No new messages.
+      return None
+
+    return self.__parsed.pop()
 
 class Message:
   # Map raw command mnemonics to a more readable / easily changeable form.
@@ -34,7 +144,24 @@ class Message:
   # in a position to handle them.
   SendSensorStatus = "ENSTAT"
 
-  def __init__(self, command, dest, *args):
+  def __init__(self, *args):
+    """ Checks the number of arguments, and defers to the proper method for
+    construction. See the following two methods for the details. """
+    if not len(args):
+      # Default-initialize everything.
+      self.__initialize_default()
+    else:
+      # Initialize with specific data.
+      self.__initialize_with_data(*args)
+
+  def __initialize_default(self):
+    """ Default method that just initializes everything to null values. """
+    self.command = ""
+    self.source = 0
+    self.dest = 0
+    self.fields = []
+
+  def __initialize_with_data(self, command, dest, *args):
     """ Represents a message to be sent over the interface.
     Args:
       command: The command mnemonic. Should be chosen from those defined above.
@@ -42,6 +169,7 @@ class Message:
             system MCU.
       All further arguments will be interpreted as fields. """
     self.command = command
+    self.source = 1
     self.dest = dest
     self.fields = args
 
@@ -58,7 +186,6 @@ class Message:
     logger.debug("Raw message: %s" % (raw))
     return raw
 
-
 class SerialTalker:
   def __init__(self, baudrate, conn=None):
     """
@@ -72,6 +199,9 @@ class SerialTalker:
     else:
       logger.info("Using user-provided connection object.")
 
+    # The parser we use to parse received messages.
+    self.__parser = _Parser()
+
   def __del__(self):
     logger.info("Closing connection.")
     self.__conn.close()
@@ -82,6 +212,21 @@ class SerialTalker:
     Message intstance. """
     message = Message(*args, **kwargs)
     self.__write(message.get_raw())
+
+  def read_response(self):
+    """ Reads a command from the serial interface.
+    Returns:
+      The command, read and parsed into a message structure, or None if no new
+      commands are available. """
+    if not self.__conn.inWaiting():
+      # No data to read.
+      return None
+
+    # Read as much data as we have available.
+    data = self.__conn.read(self.__conn.inWaiting())
+    self.__parser.parse(data)
+
+    return self.__parser.get_message()
 
   def __write(self, message):
     """ Writes a raw message.
--- python/tests/test_serial.py
+++ python/tests/test_serial.py
@@ -14,6 +14,14 @@ class TestConnection:
 
   def __init__(self):
     self.__buffer = collections.deque()
+    # The message that will be returned when we call read().
+    self.__set_message = ""
+
+  def read(self, length):
+    """ Read the set message. """
+    message = self.__set_message[:length]
+    self.__set_message = self.__set_message[length:]
+    return message
 
   def write(self, message):
     """ Writes a message to the buffer. """
@@ -21,12 +29,23 @@ class TestConnection:
 
   def close(self):
     self.__buffer.clear()
+    self.__set_message = ""
+
+  def inWaiting(self):
+    """ Get how many bytes are ready to be read. """
+    return len(self.__set_message)
 
   def get_command(self):
     """
     Returns:
       The earliest command that was written. """
     return self.__buffer.pop()
+
+  def set_message(self, message):
+    """ Set the message that will be returned when we call read.
+    Args:
+      message: The message to set. """
+    self.__set_message = message
 
 class SerialTalkerTest(unittest.TestCase):
   """ Tests for the SerialTalker class. """
@@ -35,6 +54,9 @@ class SerialTalkerTest(unittest.TestCase):
     # Create a SerialTalker with a dummy connection.
     self.__conn = TestConnection()
     self.__serial_talker = serial_talker.SerialTalker(0, conn=self.__conn)
+
+  def tearDown(self):
+    self.__conn.close()
 
   def test_simple_message(self):
     """ Tests sending a message with no fields. """
@@ -49,3 +71,63 @@ class SerialTalkerTest(unittest.TestCase):
 
     message = self.__conn.get_command()
     self.assertEqual("<TEST/12/42/None/hello>", message)
+
+  def test_simple_message_read(self):
+    """ Tests parsing a message without fields. """
+    # Make sure that, initially, we get nothing.
+    self.assertIsNone(self.__serial_talker.read_response())
+
+    self.__conn.set_message("<TEST/12>")
+
+    # Now, we should get something.
+    message = self.__serial_talker.read_response()
+    self.assertIsNotNone(message)
+    self.assertEqual("TEST", message.command)
+    self.assertEqual(1, message.source)
+    self.assertEqual(2, message.dest)
+    self.assertEqual([], message.fields)
+
+  def test_message_read_with_fields(self):
+    """ Tests parsing a message with fields. """
+    # Make sure that, initially, we get nothing.
+    self.assertIsNone(self.__serial_talker.read_response())
+
+    self.__conn.set_message("<TEST/12/42/None/hello>")
+
+    # Now, we should get something.
+    message = self.__serial_talker.read_response()
+    self.assertIsNotNone(message)
+    self.assertEqual("TEST", message.command)
+    self.assertEqual(1, message.source)
+    self.assertEqual(2, message.dest)
+    # Obviously, it's not going to know automatically how to convert stuff from
+    # strings...
+    self.assertEqual(["42", "None", "hello"], message.fields)
+
+  def test_read_partial_message(self):
+    """ Tests that it can properly parse messages that are split accross reads.
+    """
+    # Add one message, and a partial one.
+    self.__conn.set_message("<TEST/12/1><TEST/")
+
+    # We should get one message here.
+    message = self.__serial_talker.read_response()
+    self.assertIsNotNone(message)
+    self.assertEqual("TEST", message.command)
+    self.assertEqual(1, message.source)
+    self.assertEqual(2, message.dest)
+    self.assertEqual(["1"], message.fields)
+
+    # We should not get any more for now.
+    self.assertIsNone(self.__serial_talker.read_response())
+
+    # Add the rest of the message.
+    self.__conn.set_message("12/2>")
+
+    # Now it should parse correctly.
+    message = self.__serial_talker.read_response()
+    self.assertIsNotNone(message)
+    self.assertEqual("TEST", message.command)
+    self.assertEqual(1, message.source)
+    self.assertEqual(2, message.dest)
+    self.assertEqual(["2"], message.fields)
