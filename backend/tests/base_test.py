6806ae40df5745b9287751fed32280e2161065a5
Daniel Petti <djpetti@gmail.com>
2017-03-21 11:54:17 -0400

    Add better mechanism for closing the websocket.
    This fixes the issue where the order that websocket tests are run
    impacts whether they succeed or fail.

--- backend/tests/base_test.py
+++ backend/tests/base_test.py
@@ -1,8 +1,13 @@
 import logging
 
+import tornado.gen
 import tornado.testing
 
 from .. import server
+from .. import websocket
+
+
+logger = logging.getLogger(__name__)
 
 
 class _BaseTestMixin:
@@ -55,14 +60,46 @@ class BaseWebSocketTest(BaseHttpTest):
     # WebSocket URL.
     self._socket_url = "ws://localhost:%d/app_socket" % (self.get_http_port())
 
+
   def _connect_and_run(self, connect_callback, receive_callback=None):
     """ Connects to a websocket, and runs a callback once the connection has
     been initiated.
     Args:
       connect_callback: The callback to run once the socket is connected.
       receive_message: The callback to run when a message is received. """
+    def receive_wrapper(message):
+      """ A simple wrapper that ignores None messages. Otherwise, we get a None
+      message when we close the connection, which is sort of inconvenient for
+      testing.
+      Args:
+        message: The received message. """
+      if message is None:
+        return
+
+      receive_callback(message)
+
     connected = tornado.websocket.websocket_connect(self._socket_url,
-                    on_message_callback=receive_callback)
+                    on_message_callback=receive_wrapper)
     self.io_loop.add_future(connected, connect_callback)
 
     self.wait()
+
+    # Close the connection.
+    connected.result().close()
+
+    def on_wait_done(*args):
+      """ Runs until the wait period is done, and stops the IOLoop. """
+      self.stop()
+
+    # Give it some time to actually close.
+    logger.info("Waiting for connections to close...")
+    while True:
+      wait_future = tornado.gen.sleep(0.1)
+      self.io_loop.add_future(wait_future, on_wait_done)
+
+      # Do the async delay.
+      self.wait()
+
+      if not websocket.GrobotWebSocket.get_num_clients():
+        # Everything is closed.
+        break
