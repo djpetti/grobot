567858841c7eb91ab33dce2fb6fc577d2d130f02
Daniel Petti <djpetti@gmail.com>
2017-11-04 14:08:11 -0400

    Temporary fix for WCT hanging issue.
    I reverted back to 6.3.0 for now.
    
    See the issue I submitted here:
    https://github.com/Polymer/web-component-tester/issues/648

--- test_ci.sh
+++ test_ci.sh
@@ -3,4 +3,4 @@
 # Builds and runs tests inside a container for CI.
 
 docker build -t grobot-testing -f containers/testing/Dockerfile .
-docker run -v `pwd`:/grobot -w /grobot grobot-testing ./deploy.py -tc
+docker run -v `pwd`:/grobot -v /dev/shm:/dev/shm -w /grobot grobot-testing ./deploy.py -tc
